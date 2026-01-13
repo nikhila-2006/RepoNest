768a2b5738875843ca7937205ef8f664740bd5a7
NIKHILA REDDY <iamnikhilareddy@gmail.com>
2026-01-14 00:19:39 +0530

    feat: integrate AWS S3 storage and implement push command

--- Backend/controllers/push.js
+++ Backend/controllers/push.js
@@ -1,4 +1,33 @@
+const { PutObjectCommand } = require("@aws-sdk/client-s3");
+const fs=require('fs').promises;
+const path=require("path");
+const {s3}=require("../config/aws-config.js");
+
+
 async function pushRepo() {
-    console.log("Push repository");
+    const repoPath=path.resolve(process.cwd(),'.repNest');
+    const commitsPath=path.join(repoPath,'commits');
+    try{
+        const commitDirs=await fs.readdir(commitsPath);
+        for(const commitDir of commitDirs){
+            const commitPath=path.join(commitsPath,commitDir);
+            const files=await fs.readdir(commitPath);
+            for(const file of files){
+                const filePath=path.join(commitPath,file);
+                const fileContent=await fs.readFile(filePath);
+                const command = new PutObjectCommand({
+                    Bucket: process.env.S3_BUCKET_NAME,
+                    Key: `commits/${commitDir}/${file}`,
+                    Body: fileContent,
+                    Region:process.env.AWS_REGION
+                });
+
+                await s3.send(command);
+            }
+        }
+        console.log("All commits pushed to s3.");
+    }catch(err){
+        console.error("Error pushing to S3:",err.message);
+    }
 }
 module.exports={pushRepo};
